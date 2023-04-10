4b6429f40f158d13369a2dd869503579804d4f83
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-10 16:30:42 +0200

    Fixed main and section gaps

--- src/assets/js/wildersData.js
+++ src/assets/js/wildersData.js
@@ -90,7 +90,7 @@ const wilders = [
         },
     },
     {
-        name:"Oyhana Mahjoubi",
+        name:"Alexis Chesneau",
         picture:"./src/assets/images/OyhanaMahjoubi.png",
         job:"Web Developer",
         description:"",
@@ -104,6 +104,72 @@ const wilders = [
             project3:"ici",
         },
     },
+    {
+        name:"Chloé Elle",
+        picture:"./src/assets/images/OyhanaMahjoubi.png",
+        job:"Web Developer",
+        description:"",
+        skills: {
+            skill1:"Management",
+            skill2:"Comunication",
+        },
+        projects: {
+            project1:"Test",
+            project2:"1",
+            project3:"ici",
+        },
+    },
+    {
+        name:"Roxane Belahcene",
+        picture:"./src/assets/images/OyhanaMahjoubi.png",
+        job:"Web Developer",
+        description:"",
+        skills: {
+            skill1:"Management",
+            skill2:"Comunication",
+        },
+        projects: {
+            project1:"Test",
+            project2:"1",
+            project3:"ici",
+        },
+    },
+    {
+        name:"Stéphane Corbedda",
+        picture:"./src/assets/images/StephaneCorbedda.jpg",
+        job:"Web Developer",
+        description:"",
+        skills: {
+            skill1:"Management",
+            skill2:"Comunication",
+        },
+        projects: {
+            project1:"Test",
+            project2:"1",
+            project3:"ici",
+        },
+    },
+    {
+        name:"Vincent Daviaud",
+        picture:"./src/assets/images/VincentDaviaud.jpg",
+        job:"Web Developer",
+        description:"",
+        skills: {
+            skill1:"Management",
+            skill2:"Comunication",
+        },
+        projects: {
+            project1:"Test",
+            project2:"1",
+            project3:"ici",
+        },
+    },
+
+
+
+
+
+
 ]
 
 export default wilders;
